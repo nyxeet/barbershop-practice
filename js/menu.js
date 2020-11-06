31b272aa2a2b7ec97f83d7b9c79c48fa7c0953bf
nyxeet <nyxeet@ukr.net>
2020-11-06 02:57:57 +0200

    first day

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const backdropRef = document.querySelector("[backdrop-menu]");
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
@@ -10,5 +11,6 @@
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
+    backdropRef.classList.toggle("is-open");
   });
 })();
